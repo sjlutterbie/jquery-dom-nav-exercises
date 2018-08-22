ec0b115f9320cf29f4e1246b59b29140ffe7ae4d
sjlutterbie <simon@ltrbe.com>
2018-08-22 03:24:58 +0000

    Checked/unchecked toggle complete

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ function eventHandler() {
     
     // Check/uncheck items
     
+    $('ul.shopping-list').on('click', '.shopping-item-toggle', function(event){
+        
+        // Navigate DOM to label, toggle checked class
+        
+        $(this).parents('li').children('span.shopping-item')
+            .toggleClass('shopping-item__checked');
+        
+    });
+    
+            //shopping-item__checked
+    
     // Permanently remove items from the list
     
 }
