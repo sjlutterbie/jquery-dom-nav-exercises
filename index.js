3f1df7d861aceeb74e35e995127bf7e0735687f1
sjlutterbie <simon@ltrbe.com>
2018-08-22 03:08:32 +0000

    Identified key app functionality

---
+++ index.js
@@ -0,0 +1,15 @@
+// eventHandler function
+
+function eventHandler() {
+    
+    // Add items using the form
+    
+    // Check/uncheck items
+    
+    // Permanently remove items from the list
+    
+}
+
+// Launch App!
+
+$(eventHandler());
